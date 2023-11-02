790ca4f33861b14ee5d2e35cf3611fcaf147b4c3
tranhoangvy <tranhoangvy1998@gmail.com>
2023-11-02 20:40:46 +0700

    resolve conflict

--- work2.py
+++ work2.py
@@ -4,6 +4,10 @@ print(" hello thanh B")
 #thanh b code
 print("hello thanh A")
 #new code from thanh A
+<<<<<<< HEAD
 print("hello thanh A dep trai. bạn có nguoi yeu chưa")
 #commit 2
 print("chao ban cho minh lam quen hihi")
+=======
+print("hello thanh A dep trai. rất vui khi gặp bạn và được làm quen với bạn hihi")
+>>>>>>> c0ae21d (commit1 - thanhb)
