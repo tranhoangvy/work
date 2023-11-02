e1840d6ab48b8e4b405412a52fcb5ad28b115190
tranhoangvy <tranhoangvy1998@gmail.com>
2023-11-02 09:58:21 +0700

    thanhA one commit

--- work2.py
+++ work2.py
@@ -4,4 +4,6 @@ print(" hello thanh B")
 #thanh b code
 print("hello thanh A")
 #new code from thanh A
-print("hello thanh A dep trai. rất vui khi gặp bạn")
+print("hello thanh A dep trai. bạn có nguoi yeu chưa")
+#commit 2
+print("chao ban cho minh lam quen hihi")
